449e12bf6876f73215c5802dace061fb75f68e38
何小贤 <639859836@qq.com>
2016-11-01 15:06:32 +0800

    添加跳转和log的工具类

--- ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
@@ -3,9 +3,14 @@ package cn.ucai.superwechat.utils;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import com.baidu.platform.comapi.map.A;
 
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.LoginActivity;
 import cn.ucai.superwechat.ui.MainActivity;
+import cn.ucai.superwechat.ui.RegisterActivity;
 
 
 public class MFGT {
@@ -25,6 +30,12 @@ public class MFGT {
     public static void startActivity(Context context, Intent intent){
         context.startActivity(intent);
         ((Activity)context).overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+    public static void gotoLogin(Activity context){
+        startActivity(context,LoginActivity.class);
+    }
+    public static void gotoRegister(Activity context){
+        startActivity(context, RegisterActivity.class);
     }
 
 }
