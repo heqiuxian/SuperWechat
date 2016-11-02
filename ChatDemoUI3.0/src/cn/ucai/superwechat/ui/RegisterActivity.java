d9e18135df5428d858f4f9c63ef925c4e9ff7441
何小贤 <639859836@qq.com>
2016-11-02 10:11:42 +0800

    环信注册加密

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/RegisterActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/RegisterActivity.java
@@ -35,6 +35,7 @@ import cn.ucai.superwechat.bean.Result;
 import cn.ucai.superwechat.data.NetDao;
 import cn.ucai.superwechat.data.OkHttpUtils;
 import cn.ucai.superwechat.utils.CommonUtils;
+import cn.ucai.superwechat.utils.MD5;
 import cn.ucai.superwechat.utils.MFGT;
 
 /**
@@ -170,7 +171,7 @@ public class RegisterActivity extends BaseActivity {
             public void run() {
                 try {
                     // call method in SDK
-                    EMClient.getInstance().createAccount(username, pwd);
+                    EMClient.getInstance().createAccount(username, MD5.getMessageDigest(pwd));
                     runOnUiThread(new Runnable() {
                         public void run() {
                             if (!RegisterActivity.this.isFinishing())
